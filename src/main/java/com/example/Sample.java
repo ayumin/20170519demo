ed0131f789781b00c10ed86eb85f9010763627b9
Ayumu AIZAWA <ayumu.aizawa@gmail.com>
2017-05-19 08:15:30 +0900

    素数判定メソッドの追加

--- src/main/java/com/example/Sample.java
+++ src/main/java/com/example/Sample.java
@@ -4,4 +4,23 @@ public class Sample {
     public static int num() {
      return 10;
     }
+
+    public static boolean isPrimeNum(int n) {
+      int m = 0;
+
+      if (n <= 1) { return false; }
+      if (n == 2) { return true; }
+      if (n % 2 == 0) { return false; }
+
+      m = n;
+
+      for(int i = 3; i < m; ++i) {
+        if (n % i == 0) {
+          System.out.println(n + "は" + i + "で割り切れるので素数ではない");
+          return false;
+        }
+      }
+      System.out.println(n + "は素数");
+      return true;
+    }
 }
--- src/test/java/com/example/SampleTest.java
+++ src/test/java/com/example/SampleTest.java
@@ -9,4 +9,22 @@ public class SampleTest {
    public void testNum() {
       assertEquals(10, Sample.num());
    }
+
+   @Test public void test0の素数判定() { assertFalse("0は素数ではない",Sample.isPrimeNum(0)); }
+   @Test public void test1の素数判定() { assertFalse("1は素数ではない",Sample.isPrimeNum(1)); }
+   @Test public void test2の素数判定() { assertTrue("2は素数",Sample.isPrimeNum(2)); }
+   @Test public void test3の素数判定() { assertTrue("3は素数ではない",Sample.isPrimeNum(3)); }
+   @Test public void test4の素数判定() { assertFalse("4は素数ではない",Sample.isPrimeNum(4)); }
+   @Test public void test5の素数判定() { assertTrue("5は素数",Sample.isPrimeNum(5)); }
+   @Test public void test6の素数判定() { assertFalse("6は素数ではない",Sample.isPrimeNum(6)); }
+   @Test public void test7の素数判定() { assertTrue("7は素数",Sample.isPrimeNum(7)); }
+   @Test public void test8の素数判定() { assertFalse("8は素数ではない",Sample.isPrimeNum(8)); }
+   @Test public void test9の素数判定() { assertFalse("9は素数ではない", Sample.isPrimeNum(9)); }
+   @Test public void test10の素数判定() { assertFalse("10は素数ではない", Sample.isPrimeNum(10)); }
+   @Test public void test11の素数判定() { assertTrue("11は素数", Sample.isPrimeNum(11)); }
+   @Test public void test97の素数判定() { assertTrue("97は素数", Sample.isPrimeNum(97)); }
+   @Test public void test2357の素数判定() { assertTrue("2357は素数", Sample.isPrimeNum(2357)); }
+   @Test public void test99991の素数判定() { assertTrue("99991は素数", Sample.isPrimeNum(99991)); }
+   @Test public void test524287の素数判定() { assertTrue("524287は素数", Sample.isPrimeNum(524287)); }
+   @Test public void test524289の素数判定() { assertFalse("524289は素数ではない", Sample.isPrimeNum(524289)); }
 }
